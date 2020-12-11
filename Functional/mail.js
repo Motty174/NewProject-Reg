fd972c8cfc5ac4c9be4d1f970fb36aa6e2b8c5d3
Motty174 <sahakyanonik174@gmail.com>
2020-12-12 02:29:43 +0400

    x1

--- Functional/mail.js
+++ Functional/mail.js
@@ -5,8 +5,8 @@ const nodemailer=require('nodemailer')
 const mail=nodemailer.createTransport({
     host: "smtp.gmail.com",
     auth:{
-        user: require('../config/auth').email,
-        pass: require('../config/auth').emailPass
+        user: `${require('../config/auth').email}`,
+        pass: `${require('../config/auth').emailPass}`
     },
     tls: {
         // do not fail on invalid certs
@@ -14,7 +14,7 @@ const mail=nodemailer.createTransport({
       }
 })
 const mes={
-    from: require('../config/auth').email,
+    from: `${require('../config/auth').email}`,
     to: email,
     subject: 'Account verification',
     text: `http://localhost:8080/users/${number}`
